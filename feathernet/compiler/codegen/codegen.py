8c24a6b7eb7b4b2567f7e52e8ffd31060389ddb4
Camille Dunning <dunningcamille@gmail.com>
2023-12-21 20:08:21 -0800

    Forward and backward dense network codegen.

--- feathernet/compiler/codegen/codegen.py
+++ feathernet/compiler/codegen/codegen.py
@@ -4,8 +4,8 @@ from typing import Any
 
 import numpy as np
 
-from feathernet.compiler.ir_base import IRNode, ModelIR
 from feathernet.compiler.codegen.utils import np_to_cpp_array
+from feathernet.compiler.ir_base import IRNode, ModelIR
 
 
 class LayerCodeGen:
@@ -57,7 +57,9 @@ class NetworkCodeGen:
 
     def generate_network_code(self) -> str:
         common_includes = self._generate_common_includes()
-        forward_declarations = self._generate_forward_declarations()
+        forward_backward_declarations = (
+            self._generate_forward_backward_declarations()
+        )
 
         layer_codes = [
             self._generate_layer_code(node, i)
@@ -67,7 +69,7 @@ class NetworkCodeGen:
         network_code = (
             common_includes
             + "\n"
-            + forward_declarations
+            + forward_backward_declarations
             + "\n\n"
             + "\n\n".join(layer_codes)
             + "\n\n"
@@ -78,11 +80,15 @@ class NetworkCodeGen:
     def _generate_common_includes(self) -> str:
         return "#include <cassert>\n#include <iostream>\n#include <vector>\n"
 
-    def _generate_forward_declarations(self) -> str:
-        declarations = [
-            f"void layerFunction_layer{i}(const std::vector<float> &, std::vector <float> &, const std::vector<float> &, const std::vector<float> &);"
-            for i in range(len(self.model_ir.nodes))
-        ]
+    def _generate_forward_backward_declarations(self) -> str:
+        declarations = []
+        for i in range(len(self.model_ir.nodes)):
+            declarations.append(
+                f"void layerFunction_layer{i}_Forward(const std::vector<float> &, std::vector<float> &, const std::vector<float> &, const std::vector<float> &);"
+            )
+            declarations.append(
+                f"void layerFunction_layer{i}_Backward(const std::vector<float> &, const std::vector<float> &, std::vector<float> &, std::vector<float> &, std::vector<float> &, const std::vector<float> &);"
+            )
         return "\n".join(declarations)
 
     def _generate_layer_code(self, node: IRNode, layer_idx: int) -> str:
@@ -94,8 +100,8 @@ class NetworkCodeGen:
             main_template = f.read()
 
         network_initialization: list = []
-        weights_biases_init: list = []
-        network_execution: list = []
+        forward_pass: list = []
+        backward_pass: list = []
         final_output_handling: list = []
 
         for i, node in enumerate(self.model_ir.nodes):
@@ -106,6 +112,9 @@ class NetworkCodeGen:
 
             input_var = "input0" if i == 0 else f"output{i - 1}"
             output_var = f"output{i}"
+            grad_var = f"grad{i}"
+            weights_grad_var = f"weights_grad{i}"
+            bias_grad_var = f"bias_grad{i}"
 
             if i == 0:
                 network_initialization.append(
@@ -115,19 +124,37 @@ class NetworkCodeGen:
             network_initialization.append(
                 f"std::vector<float> {output_var}({output_dim}, 0.0f);"
             )
+
             weights_init = np_to_cpp_array(weights)
             bias_init = np_to_cpp_array(bias)
-
-            weights_biases_init.append(
+            network_initialization.append(
                 f"std::vector<float> weights{i} = {weights_init};"
             )
-            weights_biases_init.append(
+            network_initialization.append(
                 f"std::vector<float> bias{i} = {bias_init};"
             )
 
-            network_execution.append(
-                f"layerFunction_layer{i}({input_var}, {output_var}, weights{i}, bias{i});"
+            network_initialization.append(
+                f"std::vector<float> {grad_var}({output_dim}, 0.0f);"
             )
+            network_initialization.append(
+                f"std::vector<float> {weights_grad_var}({input_dim * output_dim}, 0.0f);"
+            )
+            network_initialization.append(
+                f"std::vector<float> {bias_grad_var}({output_dim}, 0.0f);"
+            )
+
+            forward_pass.append(
+                f"layerFunction_layer{i}_Forward({input_var}, {output_var}, weights{i}, bias{i});"
+            )
+
+            if i < len(self.model_ir.nodes) - 1:
+                next_grad_var = f"grad{i + 1}"
+                backward_pass.append(
+                    f"layerFunction_layer{i}_Backward({input_var}, {next_grad_var}, {grad_var}, weights_grad{i}, bias_grad{i}, weights{i});"
+                )
+
+        backward_pass.reverse()
 
         final_output_layer = f"output{len(self.model_ir.nodes) - 1}"
         final_output_handling.append(
@@ -137,10 +164,13 @@ class NetworkCodeGen:
 
         main_template = main_template.replace(
             "@NETWORK_INITIALIZATION @",
-            "\n  ".join(network_initialization + weights_biases_init),
+            "\n  ".join(network_initialization),
         )
         main_template = main_template.replace(
-            "@NETWORK_EXECUTION @", "\n  ".join(network_execution)
+            "@FORWARD_PASS @", "\n  ".join(forward_pass)
+        )
+        main_template = main_template.replace(
+            "@BACKWARD_PASS @", "\n  ".join(backward_pass)
         )
         main_template = main_template.replace(
             "@FINAL_OUTPUT_HANDLING @", "\n  ".join(final_output_handling)
--- feathernet/compiler/codegen/templates/dense.cpp
+++ feathernet/compiler/codegen/templates/dense.cpp
@@ -1,6 +1,7 @@
-void layerFunction(const std::vector<float> &input, std::vector<float> &output,
-                   const std::vector<float> &weights,
-                   const std::vector<float> &bias) {
+void layerFunction_Forward(const std::vector<float> &input,
+                           std::vector<float> &output,
+                           const std::vector<float> &weights,
+                           const std::vector<float> &bias) {
   const int input_dim = @INPUT_DIM @;
   const int output_dim = @OUTPUT_DIM @;
 
@@ -15,5 +16,27 @@ void layerFunction(const std::vector<float> &input, std::vector<float> &output,
       output[i] += input[j] * weights[i * input_dim + j];
     }
     output[i] += bias[i];
+  }
+}
+
+void layerFunction_Backward(const std::vector<float> &input,
+                            const std::vector<float> &output_grad,
+                            std::vector<float> &input_grad,
+                            std::vector<float> &weights_grad,
+                            std::vector<float> &bias_grad,
+                            const std::vector<float> &weights) {
+  const int input_dim = @INPUT_DIM @;
+  const int output_dim = @OUTPUT_DIM @;
+
+  input_grad.resize(input_dim, 0.0f);
+  weights_grad.resize(input_dim * output_dim, 0.0f);
+  bias_grad.resize(output_dim, 0.0f);
+
+  for (int i = 0; i < output_dim; ++i) {
+    for (int j = 0; j < input_dim; ++j) {
+      weights_grad[i * input_dim + j] += output_grad[i] * input[j];
+      input_grad[j] += output_grad[i] * weights[i * input_dim + j];
+    }
+    bias_grad[i] += output_grad[i];
   }
 }
